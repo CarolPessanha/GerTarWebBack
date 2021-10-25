a78e00deba819098f254c66bc6351fd2307ced70
Carol <Carol.andradepessanha@hotmail.com>
2021-10-25 16:00:26 -0300

    métodos: readAll, readById, delete

---
+++ web/src/routes/TaskRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const TaskController = require('../controller/TaskController');
+
+router.post('/', TaskController.create);
+router.put('/:id', TaskController.update);
+router.get('/filter/all', TaskController.readAll);
+router.get('/:id', TaskController.readById);
+router.delete('/:id', TaskController.delete);
+
+
+
+module.exports = router;
